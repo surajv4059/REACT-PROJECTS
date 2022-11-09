b4a2b28b72ae5c728f69877abd69645cd7af21cf
suraj verma <suraj12.116@gmail.com>
2022-11-09 16:29:28 +0530

    .

--- daily_goals/src/Components/Home.js
+++ daily_goals/src/Components/Home.js
@@ -1,18 +1,58 @@
-import React  from "react";
+import React, {useEffect, useState}  from "react";
 import Task from "./Task";
 
 const Home = () => {
-  return <>
+
+  const initialArray = localStorage.getItem("tasks")?JSON.parse(localStorage.getItem("tasks")):[];
+  const [tasks,setTasks] = useState(initialArray);
+  const [title,setTitle] = useState([""]);
+  const [description,setDescription] = useState([""]);
+  console.log(tasks);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setTasks([...tasks,{title,description}]);
+    setTitle("");
+    setDescription("");
+  };
+
+  const deleteTask = (index) => {
+    const filteredArray = tasks.filter((val,i) =>{
+      return i!==index;
+    });
+    setTasks(filteredArray);
+  }
+
+  useEffect(() => {
+    localStorage.setItem("tasks",JSON.stringify(tasks))
+  },[tasks])
+
+  return (
   <div className="container">
     <h1>DAILY GOALS</h1>
-    <form >
-      <input type="text" placeholder="Title" />
-      <textarea placeholder="Description"></textarea>
-      <button type="submit">ADD</button>
+    <form onSubmit={submitHandler}>
+      <input type="text" 
+      placeholder="Title" 
+      value = {title}  
+      onChange = { (e) => setTitle(e.target.value)}
+        />
+      <textarea 
+      placeholder="Description"
+       value = {description}
+         onChange = { (e) => setDescription(e.target.value)}></textarea>
+      <button type="submit" >ADD</button>
     </form>
-    <Task />
+
+    {tasks.map((item,index)=>{
+      return <Task key = {index}
+       title= {item.title}
+        description={item.description}
+        deleteTask = {deleteTask}
+        index = {index}
+        />
+    })}
   </div>
-  </>
+  )
 };
 
 export default Home;
--- daily_goals/src/Components/Task.jsx
+++ daily_goals/src/Components/Task.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const Task  = () =>{
+const Task  = ({title,description,deleteTask,index}) =>{
     return <div className='task'>
         <div>
-            <p>Title</p>
-            <span>Description</span>
+            <p>{title}</p>
+            <span>{description}</span>
         </div>
 
-        <button>-</button>
+        <button onClick={()=>deleteTask(index)}>-</button>
 
     </div>
   }
 
-export default Task
+export default Task;
